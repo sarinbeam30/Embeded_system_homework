a959c8bb3263cb13f268b8aadb82212321292394
Sarin Wanichwasin <sarin_beam30@hotmail.com>
2020-02-28 19:23:48 +0700

    third-assignment-1-2-3

--- HW_3/NUM_3/[HW4][Num 3].c
+++ HW_3/NUM_3/[HW4][Num 3].c
@@ -5,6 +5,8 @@
 #include "stm32f413h_discovery_lcd.h"
 #define PressButton 1
 
+Semaphore one_slot(1);
+
 Mutex stdio_mutex;
 Thread thread_A, thread_B, thread_C;
 DigitalIn  User_Button(PA_0,PullNone);
@@ -28,7 +30,7 @@ void generate_temp(){
     sprintf(string_temp, "%d", temp);
     screen_setup(LCD_COLOR_BLACK, LCD_COLOR_GREEN);
     BSP_LCD_DisplayStringAt(0, 80, (uint8_t *)"Temperature : ", CENTER_MODE);
-    BSP_LCD_DisplayStringAt(0, 200, (uint8_t *) string_temp, CENTER_MODE);
+    BSP_LCD_DisplayStringAt(0, 165, (uint8_t *) string_temp, CENTER_MODE);
 }
 
 void generate_humidity(){
@@ -36,7 +38,7 @@ void generate_humidity(){
     sprintf(string_humidity, "%d", humidity);
     screen_setup(LCD_COLOR_BLACK, LCD_COLOR_MAGENTA);
     BSP_LCD_DisplayStringAt(0, 80, (uint8_t *)"Humidity : ", CENTER_MODE);
-    BSP_LCD_DisplayStringAt(0, 200, (uint8_t *) string_humidity, CENTER_MODE);
+    BSP_LCD_DisplayStringAt(0, 165, (uint8_t *) string_humidity, CENTER_MODE);
 }
 
 void generate_light(){
@@ -44,14 +46,41 @@ void generate_light(){
     sprintf(string_light, "%d", light);
     screen_setup(LCD_COLOR_BLACK, LCD_COLOR_ORANGE);
     BSP_LCD_DisplayStringAt(0, 80, (uint8_t *)"Light : ", CENTER_MODE);
-    BSP_LCD_DisplayStringAt(0, 200, (uint8_t *) string_light, CENTER_MODE);
-
+    BSP_LCD_DisplayStringAt(0, 165, (uint8_t *) string_light, CENTER_MODE);
 }
+
+void generate_three_bottom(){
+    temp = rand() % (100 + 1 - 0) + 0;
+    humidity = rand() % (1000 + 1 - 100) + 100;
+    light = rand() % (10000 + 1 - 1000) + 1000;
+
+    sprintf(string_temp, "%d", temp);
+    sprintf(string_humidity, "%d", humidity);
+    sprintf(string_light, "%d", light);
+
+    BSP_LCD_SetFont(&Font12);
+
+    BSP_LCD_DrawRect(0 ,205, 50, 35);
+    BSP_LCD_DrawRect(75, 205, 85, 35);
+    BSP_LCD_DrawRect(180, 205, 60, 35);
+
+    BSP_LCD_DisplayStringAt(0, 210, (uint8_t *) "Temp : " , LEFT_MODE);
+    BSP_LCD_DisplayStringAt(0, 225, (uint8_t *) string_temp, LEFT_MODE);
+
+    BSP_LCD_DisplayStringAt(0, 210, (uint8_t *) "Humidity : " , CENTER_MODE);
+    BSP_LCD_DisplayStringAt(0, 225, (uint8_t *) string_humidity, CENTER_MODE);
+
+    BSP_LCD_DisplayStringAt(0, 210, (uint8_t *) "Light : " , RIGHT_MODE);
+    BSP_LCD_DisplayStringAt(0, 225, (uint8_t *) string_light, RIGHT_MODE);
+}
+
 
 void notify(const char* args) 
 {
-    stdio_mutex.lock();
+    
+    one_slot.wait();
     wait(1);
+    generate_three_bottom();
 
     if(User_Button.read() == PressButton && counter == 0){
         generate_temp();
@@ -71,14 +100,15 @@ void notify(const char* args)
         counter -= 2;
     }
 
-    stdio_mutex.unlock();
+    // stdio_mutex.unlock();
+    one_slot.release();
     thread_sleep_for(750);
 }
 
 void test_thread(void const *args)
 {
     while (true) 
-    {
+    { 
         printf("KAO MA YOUNG\n");
         notify((const char*)args); 
         thread_sleep_for(750);
